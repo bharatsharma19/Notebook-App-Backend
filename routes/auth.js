a0e00a44bfe3aa7bf240843c90aced6d8b051fca
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-09 21:57:27 +0530

    Security Improved

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "thisisaBh@R@1$&@#*%";
 
 router.post(
   "/createuser",
@@ -24,13 +28,25 @@ router.post(
           .status(400)
           .json({ error: "User with this email id already Exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       // Creating a new user
-      user = User.create({
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      //res.json(user);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error Occured");
