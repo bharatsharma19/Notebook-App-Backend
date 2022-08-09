5cde462294d9c7249afd57d8cd36c1d3700c88e7
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-09 17:17:26 +0530

    Create User Completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid Name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must contain 7 Characters").isLength({ min: 7 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "User with this email id already Exists" });
+    }
+    user = User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json(user);
+  }
+);
 
 module.exports = router;
