1de138adbd428a9bcb4867ecddd02d65085057df
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-09 16:01:54 +0530

    Schema Updated

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 app.get("/", (req, res) => {
   res.send("Hello Bharat");
 });
